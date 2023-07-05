68a887a69d04643c29aaceb56a7bbc76834eaece
bruna <bruna@ubuntu-minikube.myguest.virtualbox.org>
2023-07-05 10:22:05 -0300

    automatizacao da criacao de index.html

--- IaC-desenvolve/main.tf
+++ IaC-desenvolve/main.tf
@@ -17,8 +17,14 @@ resource "aws_instance" "app_server" {
   ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
   key_name = "chave-curso-iac"
+  user_data = <<-EOF
+                 #!/bin/bash
+                 cd /hom/ubuntu
+                 echo "<h1>OLÁ,MUNDO</h1>" > index.html
+                 nohup busybox httpd -f -p 8080 &
+                 EOF
   
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "TesteAWS"
   }
 }
