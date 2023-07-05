e3d6ecdceaaddd1b8a6557d8a02caa774d4bd1c8
BrunaHelua <bruna.tiburcio@outlook.com>
2023-07-05 12:43:03 -0300

    arquivos para exec do ansible-playbook

--- IaC-desenvolve/main.tf
+++ IaC-desenvolve/main.tf
@@ -17,12 +17,12 @@ resource "aws_instance" "app_server" {
   ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
   key_name = "chave-curso-iac"
-  user_data = <<-EOF
-                 #!/bin/bash
-                 cd /hom/ubuntu
-                 echo "<h1>OLÁ,MUNDO</h1>" > index.html
-                 nohup busybox httpd -f -p 8080 &
-                 EOF
+  # user_data = <<-EOF
+  #                #!/bin/bash
+  #                cd /hom/ubuntu
+  #                echo "<h1>OLÁ,MUNDO</h1>" > index.html
+  #                nohup busybox httpd -f -p 8080 &
+  #                EOF
   
   tags = {
     Name = "TesteAWS"
