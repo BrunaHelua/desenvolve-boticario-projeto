6fab5f4fe44b373ab89093e50420b02864193f4b
BrunaHelua <bruna.tiburcio@outlook.com>
2023-07-09 16:50:52 -0300

    Infos para recursos multi-region

--- terraform-desenvolve/main.tf
+++ terraform-desenvolve/main.tf
@@ -40,13 +40,14 @@ resource "aws_instance" "dev5" {
 }
 
 resource "aws_instance" "dev6" {
-    ami = ""
+    provider = aws.us-east-2
+    ami = "ami-069d73f3235b535bd"
     instance_type = "t2.micro"
     key_name = "terraform-aws"
     tags = {
         Name = "dev6"
     }   
-    vpc_security_group_ids = ["${aws_security_group.acesso_ssh.id}"]
+    vpc_security_group_ids = ["${aws_security_group.acesso_ssh-us-east-2.id}"]
 }
 
 resource "aws_s3_bucket" "dev4" {
--- terraform-desenvolve/sec-group.tf
+++ terraform-desenvolve/sec-group.tf
@@ -15,7 +15,7 @@ resource "aws_security_group" "acesso_ssh" {
 }
 
 resource "aws_security_group" "acesso_ssh-us-east-2" {
-    provider = "aws.us-east-2"
+    provider = aws.us-east-2
   name        = "acesso_ssh"
   description = "Allow SSH inbound traffic"
 
