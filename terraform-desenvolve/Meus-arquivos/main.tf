9c258d28c424c8f1f731be1387a397960873d7d1
BrunaHelua <bruna.tiburcio@outlook.com>
2023-07-10 16:34:33 -0300

    mudanca arquivos para dir meus-arquivos

--- terraform-desenvolve/Meus-arquivos/main.tf
+++ terraform-desenvolve/Meus-arquivos/main.tf
@@ -1,27 +1,94 @@
 provider "aws" {
   region  = "us-east-1"
 }
+
+provider "aws" {
+  alias = "us-east-2"
+  region  = "us-east-2"
+}
+
 resource "aws_instance" "dev" {
-    ami = "ami-053b0d53c279acc90"
+    ami = "${var.amis["us-east-1"]}"
     count = 3
     instance_type = "t2.micro"
-    key_name = "terraform-aws"
+    key_name = "${var.key_name}"
     tags = {
         Name = "dev${count.index}"
     }   
+    vpc_security_group_ids = ["${aws_security_group.acesso_ssh.id}"]
 }
-resource "aws_security_group" "acesso_ssh" {
-  name        = "acesso_ssh"
-  description = "Allow SSH inbound traffic"
 
-  ingress {
-    description      = "SSH from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["177.9.226.219/32"]
-  }
+resource "aws_instance" "dev4" {
+    ami = "${var.amis["us-east-1"]}"
+    instance_type = "t2.micro"
+    key_name = "${var.key_name}"
+    tags = {
+        Name = "dev4"
+    }   
+    vpc_security_group_ids = ["${aws_security_group.acesso_ssh.id}"]
+    depends_on = [ "${aws_s3_bucket.dev4}" ]
+}
+
+resource "aws_instance" "dev5" {
+    ami = "${var.amis["us-east-1"]}"
+    instance_type = "t2.micro"
+    key_name = "${var.key_name}"
+    tags = {
+        Name = "dev5"
+    }   
+    vpc_security_group_ids = ["${aws_security_group.acesso_ssh.id}"]
+}
+
+resource "aws_instance" "dev6" {
+    provider = aws.us-east-2
+    ami = "${var.amis["us-east-2"]}"
+    instance_type = "t2.micro"
+    key_name = "${var.key_name}"
+    tags = {
+        Name = "dev6"
+    }   
+    vpc_security_group_ids = ["${aws_security_group.acesso_ssh-us-east-2.id}"]
+    depends_on = ["aws_dynamodb_table.dynamodb-homologacao"]
+}
+
+resource "aws_instance" "dev7" {
+    provider = aws.us-east-2
+    ami = "${var.amis["us-east-2"]}"
+    instance_type = "t2.micro"
+    key_name = "${var.key_name}"
+    tags = {
+        Name = "dev7"
+    }   
+    vpc_security_group_ids = ["${aws_security_group.acesso_ssh-us-east-2.id}"]
+}
+
+resource "aws_s3_bucket" "dev4" {
+  bucket = "brunahelua-dev4"
+
   tags = {
-    Name = "acesso_ssh"
+    Name        = "brunahelua-dev4"
+  }
+}
+
+resource "aws_dynamodb_table" "dynamodb-homologacao" {
+  provider       = "${aws.us-east-2}"
+  name           = "GameScores"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "UserId"
+  range_key      = "GameTitle"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "GameTitle"
+    type = "S"
+  }
+
+  attribute {
+    name = "TopScore"
+    type = "N"
   }
 }
